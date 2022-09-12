34eba32aeb6902012d2727c9db5d959b2ef81e4d
Miguel <miguezaga2@hotmail.com>
2022-09-12 09:12:33 -0500

    add Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import Card from "./components/Card.js";
 
 function App() {
   return (
-    <div>
-      <h1>New App</h1>
-    </div>
+    <main>
+      <Card
+        title="Sedans"
+        text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+      />
+      <Card
+        title="SUVs"
+        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+      />
+      <Card
+        title="Luxury"
+        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+      />
+    </main>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+function Card({ title, text }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Card;
