b2f505d8a73917952d58dcca65e060dffae36664
Miguel <miguezaga2@hotmail.com>
2022-09-17 09:38:14 -0500

    modify component and add background color

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import { useState } from "react";
 import Card from "./components/Card.js";
+import iconLuxury from "/src/assets/icon-luxury.svg";
+import iconSedans from "/src/assets/icon-sedans.svg";
+import iconSuvs from "/src/assets/icon-suvs.svg";
+
+var cards = [
+  {
+    icon: iconSedans,
+    title: "Sedans",
+    text: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+  {
+    icon: iconSuvs,
+    title: "SUVs",
+    text: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+  },
+  {
+    icon: iconLuxury,
+    title: "Luxury",
+    text: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+  },
+];
 
 function App() {
   return (
-    <main className="component">
-      <Card
-        title="Sedans"
-        text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
-      />
-      <Card
-        title="SUVs"
-        text="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-      />
-      <Card
-        title="Luxury"
-        text="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
-      />
+    <main className="cards">
+      {cards.map(function renderCard(card, i) {
+        let id = `card--${i}`;
+        return (
+          <Card
+            key={id}
+            cname={id}
+            icon={card.icon}
+            title={card.title}
+            text={card.text}
+          />
+        );
+      })}
     </main>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
-function Card({ title, text }) {
+function Card({ cname, title, text, icon }) {
   return (
-    <div className="card">
+    <div className={`card ${cname}`}>
+      <img className="card__img" src={icon} />
       <h2 className="card__title">{title}</h2>
       <p className="card__text">{text}</p>
       <button className="card__btn">Learn More</button>
